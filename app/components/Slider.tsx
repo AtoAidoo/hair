34e01fe71d47995666ad60445c73e4153b3b4f2e
AtoAidoo <cyrilheyman4@gmail.com>
2025-04-01 00:45:13 +0000

    mm

--- app/components/Slider.tsx
+++ app/components/Slider.tsx
@@ -34,52 +34,40 @@ const slides =[
 const Slider= () => {
     const [current, setCurrent] = useState(0)
 
-    return(
+    return (
         <div className='h-[calc(100vh-80vh)] overflow-hidden'>
-            <div className="w-max h-full flex transtion-all ease-in-out duration-1000">
-                {slides.map((slide) => (
-                    <div 
-                    className={'${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row'} 
-                    key={slide.id}
+            <div className="w-max h-full flex transition- all ease-in-out duration-1000">
+                {slides.map(slide=>(
+                    <div className={'${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row}'}
+                        key={slide.id}
                     >
-                        {/* TEXT CONTAINER */}
-                        <div className="h-1/2 xl:w-1/2 xl:h-full flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center">
-                            <h2 className="text-xl lg:text-3xl 2xl:text-5xl">{slide.description}</h2>
-                            <h1 className="text-5xl lg:text-3xl 2xl:text-5xl font-semibold">{slide.title}</h1>
+                        {/* TEXT CONTAINER  */}
+                        <div className="h-1/2 xl:w-1/2 xl:h-full flex  flex-col items-center justify-center gap-8 2xl:gap-12 text-center">
+                            <h2 className="text-xl lg:text-3xl 2xl:text-5xl">{slide.descripton}</h2>
+                            <h1 className="text-5xl lg:text-6xl 2xl:text-8xl font-semibold">{slide.title}</h1>
                             <Link href={slide.url}>
                                 <button className="rounded-md bg-black text-white py-3 px-4">SHOP NOW</button>
                             </Link>
                         </div>
-                        {/* IMAGE CONTAINER */}
+                        {/* IMAGE CONTAINER  */}
                         <div className="h-1/2 xl:w-1/2 relative">
-                            <Image 
-                                src={slide.img} 
-                                alt="" 
-                                fill 
-                                sizes="100%" 
-                                className="object-cover"
-                            />
+                            <Image src={slide.img} alt="" fill sizes="100%" className="object-cover"/>
                         </div>
                     </div>
                 ))}
             </div>
-            <div className ="absolute m-auto left-1/2 buttom-8 flex gap-4">
-                {slides.map((slide, index) => (
-                    <div 
-                        className={'w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justfy-center ${
-                            current === index ? "scale-150" : ""
-                        }
-                        key={slide.id}
-                        onClick={() => setCurrent(index)}
-                    >
-                        {current === index && (
-                            <div className="w-[6px] h-[6px] bg-gray-600 rounded-full"></div>
-                        )}
-                    </div>
-                ))} 
-            </div>
+            {slides.map((slide, index) => (
+                <div 
+                    className={'w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex itnens-center justify-center ${
+                        current === index ? "scale-150" : ""
+                    }'} 
+                    key={slide.id}
+                >
+                  { current === index && (<div className='w-[6px] h-[6px] bg-gray-600 rounded'></div>)}  
+                </div>
+            ))}
         </div>
     );
 };
 
-export default Slider
+export default Slider;
