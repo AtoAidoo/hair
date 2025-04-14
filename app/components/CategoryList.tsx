835bf0e624be9ed186fe76b3d582c1adabd0e57a
AtoAidoo <cyrilheyman4@gmail.com>
2025-04-14 03:18:03 +0000

    nm

---
+++ app/components/CategoryList.tsx
@@ -0,0 +1,229 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const CategoryList = () => {
+    return (
+        <div className='px-4 overflow-x-scroll scrollbar-hide'>
+            <div className="flex gap-4 md:gap-8">
+                <Link href="/List?cat=test" className="flex-shrink-0w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+                    <div className="relative bg-slate-100 w-full h-98">
+                        <Image 
+                            src="https://images.pexels.com/photo/female-standing-with-left-hand-on-chin-1586999" 
+                            alt="" 
+                            fill 
+                            sizes="20vw" 
+                            className="object-cover"
+                        />
+                    </div>
+                    <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
+                </Link>
+                <Link href="/List?cat=test" className="flex-shrink-0w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+                    <div className="relative bg-slate-100 w-full h-98">
+                        <Image 
+                            src="https://images.pexels.com/photo/female-standing-with-left-hand-on-chin-1586999" 
+                            alt="" 
+                            fill 
+                            sizes="20vw" 
+                            className="object-cover"
+                        />
+                    </div>
+                    <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
+                </Link>
+                <Link href="/List?cat=test" className="flex-shrink-0w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+                    <div className="relative bg-slate-100 w-full h-98">
+                        <Image 
+                            src="https://images.pexels.com/photo/female-standing-with-left-hand-on-chin-1586999" 
+                            alt="" 
+                            fill 
+                            sizes="20vw" 
+                            className="object-cover"
+                        />
+                    </div>
+                    <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
+                </Link>
+                <Link href="/List?cat=test" className="flex-shrink-0w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+                    <div className="relative bg-slate-100 w-full h-98">
+                        <Image 
+                            src="https://images.pexels.com/photo/female-standing-with-left-hand-on-chin-1586999" 
+                            alt="" 
+                            fill 
+                            sizes="20vw" 
+                            className="object-cover"
+                        />
+                    </div>
+                    <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
+                </Link>
+                <Link href="/List?cat=test" className="flex-shrink-0w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+                    <div className="relative bg-slate-100 w-full h-98">
+                        <Image 
+                            src="https://images.pexels.com/photo/female-standing-with-left-hand-on-chin-1586999" 
+                            alt="" 
+                            fill 
+                            sizes="20vw" 
+                            className="object-cover"
+                        />
+                    </div>
+                    <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
+                </Link>
+                <Link href="/List?cat=test" className="flex-shrink-0w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+                    <div className="relative bg-slate-100 w-full h-98">
+                        <Image 
+                            src="https://images.pexels.com/photo/female-standing-with-left-hand-on-chin-1586999" 
+                            alt="" 
+                            fill 
+                            sizes="20vw" 
+                            className="object-cover"
+                        />
+                    </div>
+                    <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
+                </Link>
+                <Link href="/List?cat=test" className="flex-shrink-0w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+                    <div className="relative bg-slate-100 w-full h-98">
+                        <Image 
+                            src="https://images.pexels.com/photo/female-standing-with-left-hand-on-chin-1586999" 
+                            alt="" 
+                            fill 
+                            sizes="20vw" 
+                            className="object-cover"
+                        />
+                    </div>
+                    <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
+                </Link>
+                <Link href="/List?cat=test" className="flex-shrink-0w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+                    <div className="relative bg-slate-100 w-full h-98">
+                        <Image 
+                            src="https://images.pexels.com/photo/female-standing-with-left-hand-on-chin-1586999" 
+                            alt="" 
+                            fill 
+                            sizes="20vw" 
+                            className="object-cover"
+                        />
+                    </div>
+                    <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
+                </Link>
+                <Link href="/List?cat=test" className="flex-shrink-0w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+                    <div className="relative bg-slate-100 w-full h-98">
+                        <Image 
+                            src="https://images.pexels.com/photo/female-standing-with-left-hand-on-chin-1586999" 
+                            alt="" 
+                            fill 
+                            sizes="20vw" 
+                            className="object-cover"
+                        />
+                    </div>
+                    <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
+                </Link>
+                <Link href="/List?cat=test" className="flex-shrink-0w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+                    <div className="relative bg-slate-100 w-full h-98">
+                        <Image 
+                            src="https://images.pexels.com/photo/female-standing-with-left-hand-on-chin-1586999" 
+                            alt="" 
+                            fill 
+                            sizes="20vw" 
+                            className="object-cover"
+                        />
+                    </div>
+                    <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
+                </Link>
+                <Link href="/List?cat=test" className="flex-shrink-0w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+                    <div className="relative bg-slate-100 w-full h-98">
+                        <Image 
+                            src="https://images.pexels.com/photo/female-standing-with-left-hand-on-chin-1586999" 
+                            alt="" 
+                            fill 
+                            sizes="20vw" 
+                            className="object-cover"
+                        />
+                    </div>
+                    <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
+                </Link>
+                <Link href="/List?cat=test" className="flex-shrink-0w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+                    <div className="relative bg-slate-100 w-full h-98">
+                        <Image 
+                            src="https://images.pexels.com/photo/female-standing-with-left-hand-on-chin-1586999" 
+                            alt="" 
+                            fill 
+                            sizes="20vw" 
+                            className="object-cover"
+                        />
+                    </div>
+                    <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
+                </Link>
+                <Link href="/List?cat=test" className="flex-shrink-0w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+                    <div className="relative bg-slate-100 w-full h-98">
+                        <Image 
+                            src="https://images.pexels.com/photo/female-standing-with-left-hand-on-chin-1586999" 
+                            alt="" 
+                            fill 
+                            sizes="20vw" 
+                            className="object-cover"
+                        />
+                    </div>
+                    <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
+                </Link>
+                <Link href="/List?cat=test" className="flex-shrink-0w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+                    <div className="relative bg-slate-100 w-full h-98">
+                        <Image 
+                            src="https://images.pexels.com/photo/female-standing-with-left-hand-on-chin-1586999" 
+                            alt="" 
+                            fill 
+                            sizes="20vw" 
+                            className="object-cover"
+                        />
+                    </div>
+                    <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
+                </Link>
+                <Link href="/List?cat=test" className="flex-shrink-0w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+                    <div className="relative bg-slate-100 w-full h-98">
+                        <Image 
+                            src="https://images.pexels.com/photo/female-standing-with-left-hand-on-chin-1586999" 
+                            alt="" 
+                            fill 
+                            sizes="20vw" 
+                            className="object-cover"
+                        />
+                    </div>
+                    <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
+                </Link>
+                <Link href="/List?cat=test" className="flex-shrink-0w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+                    <div className="relative bg-slate-100 w-full h-98">
+                        <Image 
+                            src="https://images.pexels.com/photo/female-standing-with-left-hand-on-chin-1586999" 
+                            alt="" 
+                            fill 
+                            sizes="20vw" 
+                            className="object-cover"
+                        />
+                    </div>
+                    <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
+                </Link>
+                <Link href="/List?cat=test" className="flex-shrink-0w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+                    <div className="relative bg-slate-100 w-full h-98">
+                        <Image 
+                            src="https://images.pexels.com/photo/female-standing-with-left-hand-on-chin-1586999" 
+                            alt="" 
+                            fill 
+                            sizes="20vw" 
+                            className="object-cover"
+                        />
+                    </div>
+                    <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
+                </Link>
+                <Link href="/List?cat=test" className="flex-shrink-0w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+                    <div className="relative bg-slate-100 w-full h-98">
+                        <Image 
+                            src="https://images.pexels.com/photo/female-standing-with-left-hand-on-chin-1586999" 
+                            alt="" 
+                            fill 
+                            sizes="20vw" 
+                            className="object-cover"
+                        />
+                    </div>
+                    <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default CategoryList
--- app/components/Slider.tsx
+++ app/components/Slider.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 const slides =[
@@ -17,7 +17,7 @@ const slides =[
     {
         id: 2,
         title:"Get your product here",
-        descripton: "Sale! Up to 20% off",
+        descripton: "Sale! Up to 10% off",
         img: "https://images.pexels.com/photo/female-standing-with-left-hand-on-chin-1586999/",
         url: "/",
         bg: "bg-gradient-to-r from-pink-50 to-blue-50",
@@ -25,47 +25,76 @@ const slides =[
     {
         id: 1,
         title:"Get your best product here",
-        descripton: "Sale! Up to 20% off",
+        descripton: "Sale! Up to 40% off",
         img: "https://images.pexels.com/photo/female-standing-with-left-hand-on-chin-1586999/",
         url: "/",
         bg: "bg-gradient-to-r from-blue-50 to-yellow-50",
     },
 ]
 const Slider= () => {
-    const [current, setCurrent] = useState(0)
+    const [current, setCurrent] = useState(0);
+
+    // useEffect(() =>{ 
+    //     const interval = setInterval(() =>{
+    //         setCurrent((prev) => (prev === slides.length - 1 ? 0 : prev + 1));
+    //     }, 3000);
+
+    // return () => clearInterval(interval);
+    // }, []);
 
     return (
-        <div className='h-[calc(100vh-80vh)] overflow-hidden'>
-            <div className="w-max h-full flex transition- all ease-in-out duration-1000">
-                {slides.map(slide=>(
-                    <div className={'${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row}'}
+        <div className="h-[calc(100vh-80px)] overflow-hidden">
+            <div 
+                className="w-max h-full flex transition- all ease-in-out duration-1000"
+                style={{ transform: `translateX(-${current * 100}vw)` }}
+            >
+                {slides.map((slide) => (
+                    <div 
+                        className={`${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row`}
                         key={slide.id}
                     >
                         {/* TEXT CONTAINER  */}
-                        <div className="h-1/2 xl:w-1/2 xl:h-full flex  flex-col items-center justify-center gap-8 2xl:gap-12 text-center">
-                            <h2 className="text-xl lg:text-3xl 2xl:text-5xl">{slide.descripton}</h2>
-                            <h1 className="text-5xl lg:text-6xl 2xl:text-8xl font-semibold">{slide.title}</h1>
+                        <div className="h-1/2 xl:w-1/2 xl:h-full flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center">
+                            <h2 className="text-xl lg:text-3xl 2xl:text-5xl">
+                                {slide.descripton}
+                            </h2>
+                            <h1 className="text-5xl lg:text-6xl 2xl:text-8xl font-semibold">
+                                {slide.title}
+                            </h1>
                             <Link href={slide.url}>
-                                <button className="rounded-md bg-black text-white py-3 px-4">SHOP NOW</button>
+                                <button className="rounded-md bg-black text-white py-3 px-4">
+                                    SHOP NOW
+                                </button>
                             </Link>
                         </div>
                         {/* IMAGE CONTAINER  */}
-                        <div className="h-1/2 xl:w-1/2 relative">
-                            <Image src={slide.img} alt="" fill sizes="100%" className="object-cover"/>
+                        <div className="h-1/2 xl:w-1/2 xl:h-full relative">
+                            <Image 
+                                src={slide.img} 
+                                alt="" 
+                                fill 
+                                sizes="100%" 
+                                className="object-cover"
+                            />
                         </div>
                     </div>
                 ))}
             </div>
-            {slides.map((slide, index) => (
-                <div 
-                    className={'w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex itnens-center justify-center' ${
+            <div className="absolute m-auto left-1/2 bottom-8 flex gap-4">
+                {slides.map((slide, index) => (
+                    <div 
+                    className={`w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex itnens-center justify-center ${
                         current === index ? "scale-150" : ""
-                    }}
+                    }`}
                     key={slide.id}
-                >
-                  { current === index && (<div className='w-[6px] h-[6px] bg-gray-600 rounded'></div>)}  
-                </div>
-            ))}
+                    onClick={()=>setCurrent(index)}
+                    >
+                    { current === index && (
+                        <div className="w-[6px] h-[6px] bg-gray-600 rounded-full"></div>
+                    )}  
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
